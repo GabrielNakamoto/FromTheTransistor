13feeed410a356dbd3bc130684530e397f5b1292
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-01-05 09:49:09 -0500

    Added register printing in ram test

--- Verilog/RAM/interactive_test.cpp
+++ Verilog/RAM/interactive_test.cpp
@@ -4,19 +4,30 @@
 
 #include "VRAM8.h"
 #include "verilated.h"
-#include "verilated_vcd_c.h"
 
-static unsigned int dt = 0;
-
-
-void cycle(const std::unique_ptr<VerilatedVcdC> &trace, const std::unique_ptr<VRAM8> &top)
+void cycle(const std::unique_ptr<VRAM8> &top)
 {
 	for (int i = 0; i < 2; ++i)
 	{
 		top->clk = !top->clk;
 		top->eval();
-		trace->dump(dt++);
 	}
+}
+
+void printRegisters(const std::unique_ptr<VRAM8> &top)
+{
+	std::printf("Register\tWord\n");
+	std::printf("----------------\n");
+	for (int r = 0; r < 8; ++r)
+	{
+		top->address = r;
+		top->load = 0;
+
+		cycle(top);
+
+		std::printf("%d\t%d\n", r, static_cast<int>(top->out));
+	}
+	std::printf("\n");
 }
 
 int main(int argc, char **argv)
@@ -24,13 +35,6 @@ int main(int argc, char **argv)
 	Verilated::commandArgs(argc, argv);
 
 	std::unique_ptr<VRAM8> top { new VRAM8 };
-
-	Verilated::traceEverOn(true);
-
-	const std::unique_ptr<VerilatedVcdC> trace { new VerilatedVcdC };
-
-	top->trace(trace.get(), 99);
-	trace->open("res.vcd");
 
 	top->clk = 1;
 
@@ -43,7 +47,7 @@ int main(int argc, char **argv)
 
 		printf("\nAddress:\t\t");
 		std::cin >> address;
-		printf("Input Number (16-bits:\t");
+		printf("Input Number (16-bits):\t");
 		std::cin >> input;
 		printf("Load Bit (1/0):\t\t");
 		std::cin >> load;
@@ -52,14 +56,14 @@ int main(int argc, char **argv)
 		top->in = input;
 		top->load = load;
 
-		cycle(trace, top);
+		cycle(top);
 
-		printf("\n\nOutput (Reg %d):\t%d\n", address, static_cast<int>(top->out));
+		printf("\nOutput (Reg %d):\t%d\n\n", address, static_cast<int>(top->out));
+		printRegisters(top);
 		printf("Continue? [Y/N]:\t");
 		std::cin >> ch;
 	}
 
-	trace->close();
 	top->final();
 
 	return 0;

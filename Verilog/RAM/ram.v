81699ae811f06dd8821a812b2f30d75a54cf76e8
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-01-04 19:16:32 -0500

    Add more testing for ram modules and dmux8way, mux, mux4way and start of ram8

--- Verilog/RAM/ram.v
+++ Verilog/RAM/ram.v
@@ -1,4 +1,4 @@
-module dmux(in, sel, a, b);
+module dmux(a, b, in, sel);
 
 	input wire in, sel;
 	output wire a, b;
@@ -14,7 +14,7 @@ module dmux(in, sel, a, b);
 
 endmodule
 
-module dmux4way(in, sel, a, b, c, d);
+module dmux4way(a, b, c, d, in, sel);
 
 	input wire in;
 	input wire [1:0] sel;
@@ -44,29 +44,134 @@ module dmux4way(in, sel, a, b, c, d);
 
 endmodule
 
-/*
-module dmux8way(in, sel, a, b, c, d, e, f, g, h);
+
+module dmux8way(a, b, c, d, e, f, g, h, in, sel);
 
 	input wire in;
 	input wire [2:0] sel;
 
 	output wire a, b, c, d, e, f, g, h;
 
+	// distribute between first halfs, then use dmux4ways
+
+	wire h1, h2;
+
+	dmux d1(
+		.in(in),
+		.sel(sel[2]),
+		.a(h1),
+		.b(h2));
+
+	dmux4way d2(
+		.in(h1),
+		.sel(sel[1:0]),
+		.a(a),
+		.b(b),
+		.c(c),
+		.d(d));
+
+	dmux4way d3(
+		.in(h2),
+		.sel(sel[1:0]),
+		.a(e),
+		.b(f),
+		.c(g),
+		.d(h));
+
 endmodule
 
+module mux(out, a, b, sel);
+
+	input wire a, b, sel;
+	output wire out;
+
+	wire nsel, o1, o2;
+
+	not n1(nsel, sel);
+	and a1(o1, a, nsel);
+
+	and a2(o2, b, sel);
+
+	or of(out, o1, o2);
+
+endmodule
+
+module mux4way(out, a, b, c, d, sel);
+
+	input wire a, b, c, d;
+	input wire [1:0] sel;
+
+	output wire out;
+
+	wire h1, h2;
+
+	mux m1(h1, a, b, sel[1]);
+	mux m2(h2, c, d, sel[1]);
+
+	mux m3(out, h1, h2, sel[0]);
+
+endmodule
+
+module Register16(out, in, load, clk);
+
+	input wire [15:0] in;
+	input wire load, clk;
+
+	output wire [15:0] out;
+
+	reg [15:0] internal;
+
+	always @(posedge clk) begin
+		out <= internal;
+		if(load) begin
+			internal <= in;
+		end
+	end
+
+endmodule
 
 // 8 16-bit registers
-module RAM8(in, address, load, out);
+module RAM8(out, in, address, load, clk);
 
-	input wire in, address, load;
+	input wire [15:0] in;
+	input wire load, clk;
+	input wire [2:0] address;
+
 	output wire out;
+
+	// 16-bit registers
+	// reg [15:0] r1, r2, r3, r4, r5, r6, r7, r8;
+
+	// distributed loads
+	wire l1, l2, l3, l4, l5, l6, l7, l8;
+
+	// outputs
+	wire o1, o2, o3, o4, o5, o6, o7, o8;
 
 	// distribute load
 	// dmux 8 way
+	dmux8way distribute(	
+		.in(load),
+		.sel(address),
+		.a(l1),
+		.b(l2),
+		.c(l3),
+		.d(l4),
+		.e(l5),
+		.f(l6),
+		.g(l7),
+		.h(l8));
 
 	// calculate outputs
+	Register16 r1(o1, in, l1, clk);
+	Register16 r2(o2, in, l2, clk);
+	Register16 r3(o3, in, l3, clk);
+	Register16 r4(o4, in, l4, clk);
+	Register16 r5(o5, in, l5, clk);
+	Register16 r6(o6, in, l6, clk);
+	Register16 r7(o7, in, l7, clk);
+	Register16 r8(o8, in, l8, clk);
 
 	// select output and connect it with output
 
 endmodule
-*/
--- Verilog/RAM/testbench.cpp
+++ Verilog/RAM/testbench.cpp
@@ -2,32 +2,83 @@
 #include <memory>
 #include <map>
 
-#include "Vdmux4way.h"
+#include "Vdmux8way.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
-template<typename T>
-void evaluateModule(std::unique_ptr<T> &module, unsigned char in, uint8_t sel, int a, int b, int c, int d)
+
+/*
+void testDmux4Way(std::unique_ptr<Vdmux4way> &module)
 {
-	module->in = in;
-	module->sel = sel;
+	auto evaluateModule = [&](unsigned char in, uint8_t sel, int a, int b, int c, int d){
+		module->in = in;
+		module->sel = sel;
 
-	module->eval();
+		module->eval();
 
-	printf("Result: 	[%d, %d, %d, %d]\n", module->a, module->b, module->c, module->d);
-	printf("Expected: 	[%d, %d, %d, %d]\n\n", a, b, c, d);
+		printf("Result: 	[%d, %d, %d, %d]\n", module->a, module->b, module->c, module->d);
+		printf("Expected: 	[%d, %d, %d, %d]\n\n", a, b, c, d);
+	};
+
+	evaluateModule(1, 0, 1, 0, 0, 0);
+	evaluateModule(1, 1, 0, 1, 0, 0);
+	evaluateModule(1, 2, 0, 0, 1, 0);
+	evaluateModule(1, 3, 0, 0, 0, 1);
+}
+*/
+
+/*
+void testDmux8Way(std::unique_ptr<Vdmux8way> &m)
+{
+	auto evaluateModule = [&](unsigned char in, uint16_t sel, std::vector<int> exp)
+	{
+		m->in = in;
+		m->sel = sel;
+
+		m->eval();
+
+		printf("Input:\t\t%d\nSelected:\t%d\n\n", in, sel);
+		printf("Result:\t\t[%d, %d, %d, %d, %d, %d, %d, %d]\n", m->a, m->b, m->c, m->d, m->e, m->f, m->g, m->h);
+		printf("Expected:\t[%d, %d, %d, %d, %d, %d, %d, %d]\n\n", exp[0], exp[1], exp[2], exp[3], exp[4], exp[5], exp[6], exp[7]);
+	};
+
+	evaluateModule(1, 0, { 1, 0, 0, 0, 0, 0, 0, 0 });
+}
+*/
+
+void interactiveD8W(std::unique_ptr<Vdmux8way> &m)
+{
+	char	ch = 'y';
+	while(tolower(ch) == 'y')
+	{
+		unsigned int 	in;
+		uint16_t 		sel;
+
+		printf("Input bit:\t\t");
+		std::cin >> in;
+		printf("Selection [0->7]:\t");
+		std::cin >> sel;
+
+		m->in = static_cast<unsigned char>(in);
+		m->sel = sel;
+
+		m->eval();
+
+		printf("\nInput:\t\t%d\nSelected:\t%d\n\n", in, sel);
+		printf("Result:\t\t[%d, %d, %d, %d, %d, %d, %d, %d]\n", m->a, m->b, m->c, m->d, m->e, m->f, m->g, m->h);
+		printf("Continue? [Y/N]\n");
+		std::cin >> ch;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	Verilated::commandArgs(argc, argv);
 
-	std::unique_ptr<Vdmux4way> top { new Vdmux4way };
+	std::unique_ptr<Vdmux8way> top { new Vdmux8way };
 
-	evaluateModule<Vdmux4way>(top, 1, 0, 1, 0, 0, 0);
-	evaluateModule<Vdmux4way>(top, 1, 1, 0, 1, 0, 0);
-	evaluateModule<Vdmux4way>(top, 1, 2, 0, 0, 1, 0);
-	evaluateModule<Vdmux4way>(top, 1, 3, 0, 0, 0, 1);
+	interactiveD8W(top);
+	// testDmux8Way(top);
 
 	top->final();
 
